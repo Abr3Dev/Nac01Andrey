d4c7168ebc7efb698d96eec27f2aede0314e6ab8
Raphael Molina <raphael.bedran@konduto.com>
2020-04-09 16:08:04 -0300

    Tentativa de implementação de js
    Utilização de alguns dados do json, implementados
    no front-end.

--- js.js
+++ js.js
@@ -14,10 +14,30 @@ function carregaDados(){
         }
 function colocaNaTela(obj) {
     for (let i = 0; i < 3; i++) { //for (let i = 0; i < obj.length; i++) {
-        let card = document.querySelectorAll('.card-title')
-        card[i].innerText = obj[i].address.formattedAddress
-        let fotos = document.querySelectorAll('.d-block.w-100').src
-        console.log(fotos);
+        let indice = i + 1;
+        let id = document.getElementById(indice.toString());
+        let end = obj[i].address.formattedAddress;
+        let imgs = obj[i].images;
+        let area = obj[i].usableArea;
+        let bed = obj[i].bedrooms;
+        let price = obj[i].price / 100;
+        document.querySelectorAll('.card-title')[i].textContent = end;
+        for (let j = 0; j < 3; j++) { //obj[i].images.length
+            document.querySelectorAll('.d-block.w-100')[i].src = imgs[j];
+        }
+        document.querySelectorAll('.area')[i].textContent = area;
+        document.querySelectorAll('.bed')[i].textContent = bed;
+        document.querySelectorAll('.price')[i].textContent = price;
+
+
+        // let end = document.querySelectorAll('.card-title');
+        // end[i].innerText = obj[i].address.formattedAddress;
+        // let area = document.querySelectorAll('');
+        // area[i].innerText = obj[i].usableArea;
+        // for (let j = 0; j < 3; j++) {
+        //     let fotos = document.querySelectorAll('.d-block.w-100');
+        //     console.log(fotos[j].src);
+        // }
         // let foto = card.querySelector('.d-block.w-100').src = obj[i].images
         // console.log(foto);
     //     let card = document.querySelector('.modal-body');
