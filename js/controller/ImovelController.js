0d831991c6ec886f881e8f2adb249c1f89612cfd
Raphael Santantonio <47859154+raphaelcamar@users.noreply.github.com>
2020-04-12 18:38:57 -0300

    retirando arquivos desnecessários e arrumando a ordem de cards

--- js/controller/ImovelController.js
+++ js/controller/ImovelController.js
@@ -1,66 +1,43 @@
-window.onload = carregaDados();
-let div = document.querySelector('.valores');
+class ImovelController{
+    constructor(){
+        this.retornaValores();
+        this.lista = new ListaImoveis();
 
-function carregaDados() {
-    console.log('chamou o método')
-    let xhr = new XMLHttpRequest();
-    xhr.open('get', 'http://www.mocky.io/v2/5e8bbc982f00006d0088c4ed');
-    xhr.send()
-    xhr.addEventListener('load', function() {
-        if (xhr.status == 200) {
-            let json = JSON.parse(xhr.responseText);
-            //ordena preco decrescente.
-            json.sort(function(a, b) {
-                if (a.price < b.price) {
-                    return 1; //mais 1
-                }
-                if (a.price > b.price) {
-                    return -1; //menos 1
-                }
+    }
 
-                return 0;
-            });
-            //ordena preco  crescente
-            json.sort(function crescente(a, b) {
-                if (a.price < b.price) {
-                    return 1; //mais 1
-                }
-                if (a.price > b.price) {
-                    return -1; //menos 1
-                }
+    retornaValores(){
+        let xhr = new XMLHttpRequest;
+        xhr.open('GET', 'http://www.mocky.io/v2/5e8bbc982f00006d0088c4ed');
+       xhr.send(null)
+        xhr.onload = function(){
 
-                return 0;
-            });
+            let jsson = JSON.parse(xhr.responseText);
+            let lista = new ListaImoveis();
+            let view = new ImovelView(document.querySelector('.cards-aps'))
+            let view2 = new ImovelView(document.querySelector('.modais'))
+            let org = new Organizar();
+           
+            jsson =  org._organizar(jsson);
+        
+           
+            jsson.forEach(function(imoveis) {
+				let i = new Imovel();
+             i.id = imoveis.id;
+                 i.estacionamento = imoveis.parkingSpaces;
+                     i.preco = imoveis.price;
+                         i.publicavel = imoveis.publish;
+                             i.area = imoveis.usableArea;
+                         i.banheiros = imoveis.bathrooms;
+                     i.quartos = imoveis.bedrooms;
+                 i.imagens = imoveis.images;
+             i.endereco = imoveis.address.formattedAddress;
 
-            //pegando valor para fazer os fluxos
-            var valPreco = document.getElementById("inputPreco");
-            var valQuarto = document.getElementById("inputQuarto");
-            var valBanheiros = document.getElementById("inputBanheiros");
-            var valVagas = document.getElementById("inputVagas");
-            console.log('==', valPreco.value, '=', valQuarto.value, '=', valBanheiros.value, '=', valVagas.value);
-
-            if (valPreco.value = 'menor') {
-                //chama funcao crescente
-            }
-
-            //=================
-            json = json.filter(function(value) { //retira valores indesejáveis.
-                return value.price !== 0 && value.publish !== false;
-            });
-            console.log(json)
-            for (let i = 0; i < json.length; i++) {
-                Imovel.id = json[i].id
-                Imovel.endereco = json[i].address.formattedAddress
-                Imovel.imagens = json[i].images
-                Imovel.preco = json[i].price / 100
-                Imovel.banheiros = json[i].bathrooms
-                Imovel.quartos = json[i].bedrooms
-                Imovel.estacionamentos = json[i].parkingSpaces
-                Imovel.area = json[i].usableArea
-                Imovel.publicavel = json[i].publish
-                div.innerHTML += template(Imovel)
-                console.log(div)
-            }
-        }
-    })
+             lista._adiciona(i)
+             view.update(lista);
+             
+             view2.updateInfos(lista, i.imagens)
+			})
+        };
+    }
+   
 }
--- js/model/Imovel.js
+++ js/model/Imovel.js
@@ -1,12 +1,13 @@
-let Imovel = {
-    id : 0,
-    endereco : '',
-    imagens : '',
-    preco : 0,
-    banheiros : 0,
-    quartos : 0,
-    estacionamentos : 0,
-    area : 0,
-    publicavel : true
-    
+class Imovel {
+    constructor(id, endereco, imagens, preco, banheiros, quartos, estacionamento, area, publicavel){
+        this.id = id;
+        this.endereco = endereco;
+        this.imagens = imagens;
+        this.preco = preco;
+        this.banheiros = banheiros;
+        this.quartos = quartos;
+        this.estacionamento = estacionamento;
+        this.area = area;
+        this.publicavel = publicavel;
+    }
 }
