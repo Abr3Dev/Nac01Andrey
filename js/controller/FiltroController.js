310beff2525593040c3fe3d482946411d13bf3cd
Raphael Santantonio <47859154+raphaelcamar@users.noreply.github.com>
2020-04-16 08:50:35 -0300

    retirando pequeno erro

--- js/controller/FiltroController.js
+++ js/controller/FiltroController.js
@@ -3,7 +3,6 @@ class FiltroController{
         this._resetarFiltro()
         this._valores = new InputValores()._valoresFiltro();
         this._aplicarFiltro = new AplicarFiltro(this._valores[0], this._valores[1], this._valores[2])
-        new PaginacaoController();
     } 
     _resetarFiltro(){
         let retira = document.querySelector('.cards-aps')
